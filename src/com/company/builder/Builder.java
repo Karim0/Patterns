8490d75fe61c711968256ecd8828bc9575a67042
Karim <karim20.ks@gmail.com>
2019-07-28 21:23:07 +0600

    Добавлен паттерн "Строитель"

---
+++ src/com/company/builder/Builder.java
@@ -0,0 +1,14 @@
+package com.company.builder;
+
+
+import com.company.builder.components.*;
+
+
+public interface Builder {
+    void setType(Type type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+}
