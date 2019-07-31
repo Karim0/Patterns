d4efa6e44a05aed2322794a40446fb290e82d53d
Karim <karim20.ks@gmail.com>
2019-07-31 15:45:18 +0600

    Добавлен паттерн "Прототип"

---
+++ src/com/company/prototype/shapes/Shape.java
@@ -0,0 +1,29 @@
+package com.company.prototype.shapes;
+
+import java.util.Objects;
+
+public abstract class Shape {
+    public int x;
+    public int y;
+    public String color;
+
+    public Shape(){}
+
+    public Shape(Shape target){
+        if(target != null){
+            x = target.x;
+            y = target.y;
+            color = target.color;
+        }
+    }
+
+    public abstract Shape clone();
+
+    @Override
+    public boolean equals(Object o){
+        if (!(o instanceof Shape)) return false;
+        Shape shape2 = (Shape) o;
+        return shape2.x == x && shape2.y == y && Objects.equals(shape2.color, color);
+
+    }
+}
